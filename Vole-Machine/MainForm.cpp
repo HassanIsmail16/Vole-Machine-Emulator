b3c6bc700e6c37209462c349998edfebe3e809ab
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2024-10-23 23:56:13 +0300

    setup project

--- Vole-Machine/MainForm.cpp
+++ Vole-Machine/MainForm.cpp
@@ -1,2 +1,12 @@
 #include "MainForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	VoleMachine::MainForm form;
+	Application::Run(% form);
+	return 0;
+}
